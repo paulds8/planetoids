b1c9ef0697fb42267417a2e1ce5ddeb51280ca08
Paul <paulds8@hotmail.com>
2019-09-07 14:10:04 +0200

    Add initial plotting setup
    Still need to decide on how much we'll allow a user to modify directly

--- planetoids/planetoids.py
+++ planetoids/planetoids.py
@@ -19,7 +19,19 @@ from functools import reduce
 from plotly.subplots import make_subplots
 
 class Planetoid(object):
-    def __init__(self, data, component1_field, component2_field, cluster_field):
+    def __init__(self,
+                 data,
+                 component1_field,
+                 component2_field,
+                 cluster_field,
+                 ecology):
+        
+        self.data = None
+        self.component1_field = None
+        self.component2_field = None
+        self.cluster_field = None
+        self.ecology = None
+        
         if isinstance(data, pd.DataFrame):
             self.data = data
         else:
@@ -36,12 +48,21 @@ class Planetoid(object):
             self.cluster_field = cluster_field
         else:
             raise ValueError('Cluster field not in provided DataFrame')
+        try:
+            cm.get_cmap(ecology, 1)
+            self.ecology = ecology
+        except Exception as e:
+            raise ValueError(e)
 
         # only keep what we need
         self.data = self.data[[component1_field, component2_field, cluster_field]]
         
         #set the rest
         self.contours = None
+        self.ocean_colour = None
+        self.fig = None
+        self.cmap = None
+        self.max_contour = None
 
 
     def rescale_coordinates(self):
@@ -118,13 +139,254 @@ class Planetoid(object):
         contours = {}
         for cluster in np.unique(self['Cluster'].values):
             points_df = self.data.loc[self.data['Cluster'] == cluster, ['Longitude', 'Latitude']]
-            contours[cluster] = get_contours(points_df)
+            contours[cluster] = get_contours()
         
         self.contours = contours
         
         
     def fit(self):
+        """Generate data required for terraforming"""
         #transform 2d components into pseudo lat/longs
         self.rescale_coordinates()
         #generate contours per class
         self.get_all_contours()
+        
+    
+    def plot_surface(self):
+        """This plots the surface layer which we need because we can't set it directly"""
+        #globe
+        self.fig.add_trace(
+            go.Scattergeo(
+                lon = [179.9,0,0,179.9],
+                lat = [89.9,89.9,0,0],
+                mode='lines',
+                line=dict(width=0, color=self.ocean_colour),
+                fill='toself',
+                fillcolor = self.ocean_colour,
+                hoverinfo='skip',
+                opacity=1,
+                showlegend=False
+            ),
+            row=1,
+            col=1
+            )
+
+        #flat
+        self.fig.add_trace(
+            go.Scattergeo(
+                lon = [179.9,0,0,179.9],
+                lat = [89.9,89.9,0,0],
+                mode='lines',
+                line=dict(width=0, color=self.ocean_colour),
+                fill='toself',
+                fillcolor = self.ocean_colour,
+                hoverinfo='skip',
+                opacity=0.95,
+                showlegend=False
+           ),
+           row=2,
+           col=1
+           )
+        
+    
+    def plot_contours(self):
+        """Plot the topography"""
+        #globe
+        for cluster, contour in self.contours.items():
+            for ix, line in enumerate(contour):
+                #need to update this to actually check for contours that form polygons
+                if len(line) > 0 and ix > 3:
+                    for l in line:
+                        if ix % 2 == 0:
+                            self.fig.add_trace(
+                                go.Scattergeo(
+                                    lon = list(l[:, 0]),
+                                    lat = list(l[:, 1]),
+                                    hoverinfo='skip',
+                                    mode='lines',
+                                    line=dict(width=0,
+                                            color='rgb' + str(cmap(ix/self.max_contour, bytes=True)[0:3])
+                                            ),
+                                    fill='toself',
+                                    fillcolor = 'rgb' + str(cmap(ix/self.max_contour, bytes=True)[0:3]),
+                                    opacity=1,
+                                    showlegend=False,
+                                    ),row=1,col=1)
+                        else:                    
+                            self.fig.add_trace(
+                                go.Scattergeo(
+                                    lon = list(l[:, 0]),
+                                    lat = list(l[:, 1]),
+                                    hoverinfo='skip',
+                                    mode='lines',
+                                    line=dict(width=0, #*np.power(np.exp(ix/max_contour),2),
+                                            #dash='longdashdot',
+                                            color='rgb' + str(cmap(ix/self.max_contour, bytes=True)[0:3])),
+                                    opacity=1,
+                                    showlegend=False
+                                    ),row=1,col=1)
+                            
+        #flat
+        for cluster, contour in contours.items():
+            for ix, line in enumerate(contour):
+                #need to update this to actually check for contours that form polygons
+                if len(line) > 0 and ix > 3:
+                    for l in line:
+                        if ix % 5 == 0:
+                            fig.add_trace(
+                                go.Scattergeo(
+                                    lon = list(l[:, 0]),
+                                    lat = list(l[:, 1]),
+                                    hoverinfo='skip',
+                                    mode='lines',
+                                    line=dict(width=0.5,
+                                              color='rgb' + str(tuple(list(cmap(ix/len(contour), bytes=True)[0:3]) + [0.5]))
+                                              ),
+                                    fill='toself',
+                                    showlegend=False,
+                                    opacity=0.95
+                                    ),row=2,col=1)
+                            
+                            
+    def plot_clustered_points(self):
+        """Plot the provided point data"""
+        self.fig.add_trace(go.Scattergeo(
+            lon = self.data['Longitude'],
+            lat = self.data['Latitude'],
+            marker_color=self.data['Cluster'],
+            hoverinfo='text',
+            hovertext=self.data['Cluster'],
+            marker_size=3,
+            showlegend= False
+        #     marker = dict(
+        #         symbol='circle-open',
+        #      )
+            ),row=1,col=1)
+
+        #plot the points - flat
+        self.fig.add_trace(go.Scattergeo(
+            lon = self.data['Longitude'],
+            lat = self.data['Latitude'],
+            marker_color=self.data['Cluster'],
+            hoverinfo='text',
+            hovertext=self.data['Cluster'],
+            marker_size=1,
+            showlegend=False,
+        #     marker = dict(
+        #         symbol='circle-open',
+        #      )
+            ),row=2,col=1)
+    
+    
+    def update_geos(self):
+        """Update config for maps"""
+        # globe
+        self.fig.update_geos(
+            row=1,col=1,
+            showland = False,
+            showcountries = False,
+            showocean = False,
+            showcoastlines=False,
+            showframe=False,
+            showrivers=False,
+            showlakes=False,
+            showsubunits=False,
+            #junk='',
+            bgcolor = "black", #changes the whole graph background, but we only want the surface
+            projection = dict(
+                type = 'orthographic',
+                rotation = dict(
+                    lon = 0,
+                    lat = 0,
+                    roll = 0
+                )
+            ),
+            lonaxis = dict(
+                showgrid = False,
+                gridcolor = 'rgb(102, 102, 102)',
+                gridwidth = 1
+            ),
+            lataxis = dict(
+                showgrid = False,
+                gridcolor = 'rgb(102, 102, 102)',
+                gridwidth = 1
+            ))
+        #flat
+        self.fig.update_geos(
+            row=2,col=1,
+            showland = False,
+            showcountries = False,
+            showocean = False,
+            showcoastlines=False,
+            showframe=False,
+            showrivers=False,
+            showlakes=False,
+            showsubunits=False,
+            #junk='',
+            bgcolor = "black", #changes the whole graph background, but we only want the surface
+            projection = dict(
+                type = 'natural earth',
+                rotation = dict(
+                    lon = 0,
+                    lat = 0,
+                    roll = 0
+                )
+            ),
+            lonaxis = dict(
+                showgrid = True,
+                gridcolor = 'rgb(102, 102, 102)',
+                gridwidth = 1
+            ),
+            lataxis = dict(
+                showgrid = True,
+                gridcolor = 'rgb(102, 102, 102)',
+                gridwidth = 1
+            ))
+        
+    
+    def update_layout(self):
+        
+        self.fig.update_layout(
+            autosize=True,
+            width=int(1920/2),
+            height=int(1281/2),
+            title_text = 'Planetoids',
+            showlegend = True,
+            plot_bgcolor = "black",
+            paper_bgcolor = "black",
+            margin=dict(l=20, r=20, t=40, b=20)
+        )
+    
+    
+    def terraform(self, plot_topography=True, plot_points=True, render=True):
+        """Construct a new world"""
+        
+        self.fig = make_subplots(
+            rows=2, cols=2, 
+            vertical_spacing=0.05,
+            column_widths=[0.5, 0.5],
+            row_heights=[0.66, 0.33],
+            specs=[[{"type": "scattergeo", "colspan": 2}, None],
+                   [{"type": "scattergeo", "colspan": 2}, None]]
+            )
+        
+        #identify the maximum number of contours per continent
+        self.max_contour = max([len(contour) for contour in self.contours.values()])
+        self.cmap = cm.get_cmap(self.ecology, max_contour)
+        
+        self.ocean_colour = 'rgb' + str(cmap(3/max_contour, bytes=True)[0:3])
+        
+        self.plot_surface()
+
+        if plot_topography:
+            self.plot_contours()
+            
+        if plot_points:
+            self.plot_clustered_points()
+                            
+        self.update_geos()
+
+        self.update_layout()
+
+        if render:
+            self.fig.show()
